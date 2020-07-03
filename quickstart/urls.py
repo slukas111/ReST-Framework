85fea79b6fdc98f91ee2d2a5e012306f215bddb8
Sasha Lukas <sashamati@gmail.com>
2020-07-03 11:17:17 -0700

    finished the project.

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from quickstart.views import *
+# from quickstart.views import *
 
 from quickstart import views
 
@@ -18,6 +18,9 @@ router.register(r"shoe", views.ShoeViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include('rest_framework.urls', namespace="rest_framework")),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework'))
 ]
+
